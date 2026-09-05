49bca05a3e506c47610e3fc05273b9d070bb7f59
timcgha <official.signup.john@gmail.com>
2026-09-05 11:27:55 +0300

    test: migrate stage 2 release assertion

--- tests/level4-stage2.test.js
+++ tests/level4-stage2.test.js
@@ -22,7 +22,7 @@ function flyTo(H,x,y,z,frames){
 // ---- version ----
 {
   const H=boot();
-  ok(H.getCamDiag().VERSION_BASE==='v53 · Desert Level 5','version stamp v53');
+  ok(H.versionUsesCanonicalRelease(),'version stamp derives from canonical BELLHOP_RELEASE');
 }
 
 // ---- Snoozle 1 ----
